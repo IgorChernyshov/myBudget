f28939123d8aa5049f08efd8682b59c363dff316
Igor Chernyshov <ichernyshov@bk.ru>
2023-12-19 11:37:55 +0300

    Initial Commit

---
+++ MyBudget/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyBudget
+//
+//  Created by Игорь Чернышов on 19.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyBudget/MyBudgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyBudgetApp.swift
+//  MyBudget
+//
+//  Created by Игорь Чернышов on 19.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct MyBudgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
